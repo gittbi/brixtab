32e1ca26d1b31dcb160f077ce61ed3d97e2bdda0
Gitte Stokvad Brix <gitte@MacBook-Pro-tilhrende-Gitte.local>
2023-09-20 10:54:41 +0200

    add meantable

---
+++ R/meantable.R
@@ -0,0 +1,21 @@
+#' meantable()
+#'
+#' @param x = dataframe
+#' @param colvar 
+#' @param rowvar 
+#' @param statvar 
+#'
+#' @return a table
+#' @export
+#'
+#' @examples meantable(fl, origin, carrier, air_time)
+#' @importFrom reshape2 acast
+#' @import dplyr
+meantable <- function(x, colvar, rowvar, statvar) {
+  df <- x %>% 
+    group_by({{colvar}}, {{rowvar}}) %>% 
+    summarise(avg_statvar = mean({{statvar}}, na.rm = TRUE), .groups = "drop")
+  # df
+  array <- acast(df, df[[1]] ~ df[[2]], value.var = "avg_statvar")
+  as.table(array)
+}
